97e4b2c3cb49de64545041e8c3e65defb2559d13
Victor <crabcore55@rambler.ru>
2022-07-18 23:15:40 +0600

    config postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5431,
+      username: 'postgres',
+      password: '12345',
+      database: 'nestjs'
+    }),
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
